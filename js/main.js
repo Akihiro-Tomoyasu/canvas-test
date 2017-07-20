5e6cc9fd913dcc8ab8d128b9faeffc850f7a7f65
Akihiro-Tomoyasu <30279753+Akihiro-Tomoyasu@users.noreply.github.com>
2017-07-20 16:36:54 +0900

    1707201636
    とりあえず作成

--- js/main.js
+++ js/main.js
@@ -1,48 +1,90 @@
 window.addEventListener('load', draw, false);
 
 function draw(){
-	var r = 50; //振幅
-	var T  = 1900; // 周期
-	var degree = 0; //角度
+	var lineColor; //色
+	var lineH; //線幅
+	var T; // 周期
+	var speed; //速度
 
+	var heightRatio = 10; //高さ
 	var w = document.documentElement.clientWidth;
-	var h = 340;
-	
-	//リサイズ
-	window.addEventListener('resize',function(){
-		w = document.documentElement.clientWidth;
-		T = w / 2
-	},false);
+	var h = w / heightRatio;
 	
 	var canvas = document.getElementById('testCanvas');
 	canvas.width = w
 	canvas.height = h;
+	
+	//リサイズ
+	window.addEventListener('resize',function(){
+		w = document.documentElement.clientWidth;
+		h = w / heightRatio;
+		canvas.width = w;
+		canvas.height = h;
+	},false);
 	
 	
 	var ctx = canvas.getContext("2d");
 
 	function loop(){ //繰り返される関数
 		ctx.clearRect(0, 0, w, h);
-		ctx.beginPath();
-		ctx.strokeStyle = 'green';
-		ctx.lineWidth = 5;
-		
-		drawWave(degree);
-		
-		ctx.stroke();
-		degree += 10;
-		
+		wave1();
+		wave2();
+		wave3();
 		requestAnimationFrame(loop);
-	}
+	};
 
 	loop();
 
+	var degree = 0;
+	function wave1(){
+		lineColor = 'red'; //色
+		lineH = 2; //線幅
+		T = 2; // 周期
+		speed = 10; //速度
+		
+		ctx.beginPath();
+		ctx.strokeStyle = lineColor;
+		ctx.lineWidth = lineH;
+		drawWave(degree);
+		ctx.stroke();
+		degree += speed;
+	}
+	
+	var degree2 = 0;
+	function wave2(){
+		lineColor = 'red'; //色
+		lineH = 2; //線幅
+		T = 3.5; // 周期
+		speed = 10; //速度
+		
+		ctx.beginPath();
+		ctx.strokeStyle = lineColor;
+		ctx.lineWidth = lineH;
+		drawWave(degree2);
+		ctx.stroke();
+		degree2 += speed;
+	}
+
+	var degree3 = 0;
+	function wave3(){
+		lineColor = 'red'; //色
+		lineH = 2; //線幅
+		T = 5; // 周期
+		speed = 10; //速度
+		
+		ctx.beginPath();
+		ctx.strokeStyle = lineColor;
+		ctx.lineWidth = lineH;
+		drawWave(degree3);
+		ctx.stroke();
+		degree3 += speed;
+	}
+
 	function drawWave(degree) {
-		var shiten = -r * Math.sin((2 * Math.PI / T) * degree) + (h / 2);
+		var shiten = (-1 * (h / 2 - lineH)) * Math.sin((2 * Math.PI / (w * T)) * degree) + (h / 2); //始点 = 幅振 * sinうんたら
 		ctx.moveTo(0, shiten); //始点
 		for (var x = 0; x < w; x++) {
-			var y = -r * Math.sin((2 * Math.PI / T) * (degree + x));
-			console.log('y',y);
+			var y = (-1 * (h / 2 - lineH)) * Math.sin((2 * Math.PI / (w * T)) * (degree + x));
 			ctx.lineTo(x, y + (h / 2));
 		}
 	}
